beb750ee710c68609795c85b8f97814e8697b41f
MilenaCastilloC <sm.castillo@uniandes.edu.co>
2022-11-05 19:08:00 -0500

    Configuración metodos controller

--- src/receta-cultura-gastronomica/receta-cultura-gastronomica.controller.ts
+++ src/receta-cultura-gastronomica/receta-cultura-gastronomica.controller.ts
@@ -1,7 +1,26 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Delete, Get, HttpCode, Param, Post, Put, UseInterceptors } from '@nestjs/common';
+import { plainToInstance } from 'class-transformer';
+import { HasRoles } from '../user/has-roles.decorator';
+import { JwtAuthGuard } from '../auth/guards/jwt-auth.guard';
+import { RolesGuard } from '../auth/guards/roles.guard';
+import { CulturaGastronomicaDto } from '../cultura-gastronomica/cultura-gastronomica.dto';
+import { CulturaGastronomicaEntity } from '../cultura-gastronomica/cultura-gastronomica.entity';
+import { BusinessErrorsInterceptor } from '../shared/interceptors/business-errors.interceptor';
 import { RecetaCulturaGastronomicaService } from './receta-cultura-gastronomica.service';
+import { Role } from '../user/role.enum';
 
-@Controller('receta-cultura-gastronomica')
+@Controller('recetas')
+@UseInterceptors(BusinessErrorsInterceptor)
+@UseGuards(JwtAuthGuard,RolesGuard)
+
 export class RecetaCulturaGastronomicaController {
-    constructor(private readonly museumService: RecetaCulturaGastronomicaService) {}
+    constructor(private readonly restauranteCulturaGastronomicaService: RecetaCulturaGastronomicaService){}
+
+    @Post(':recetaId/culturasGastronomicas/:culturaGastronomicaId')
+    @HasRoles(Role.UserWrite)
+    async addCulturaGastronomicaReceta(@Param('recetaId') recetaId: string, @Param('culturaGastronomicaId') culturaGastronomicaId: string){
+        return await this.recetaCulturaGastronomicaService.addCulturaGastronomicaReceta(recetaId, culturaGastronomicaId);
+    }
 }
+
+
